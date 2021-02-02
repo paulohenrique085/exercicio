3972e0f02fc502e648904661f1071177f7bd6c86
paulohenrique085 <paulohnq85@outlook.com>
2021-02-01 22:53:10 -0300

    aprendi

---
+++ gorgeta.js
@@ -0,0 +1,11 @@
+//função
+let calculo = () => {
+
+    //let como se fosse o var do dart
+    let contabruta = document.getElementById("comg").value
+    let contaGorgeta = document.getElementById("semg").value
+
+    console.log(contaGorgeta)
+    console.log(contabruta)
+
+}
